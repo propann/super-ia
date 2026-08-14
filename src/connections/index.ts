35f292c5fcaa8f5a26b91aba65bb8b32fc2198d2
Enzo <54742679+propann@users.noreply.github.com>
2026-08-15 01:24:56 +0200

    feat: export network policy and probe modules

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -2,5 +2,7 @@ export * from "./types.js";
 export * from "./catalog.js";
 export * from "./store.js";
 export * from "./dashboard.js";
+export * from "./network-policy.js";
+export * from "./probe.js";
 export * from "./secret-backends.js";
 export * from "./cli.js";
