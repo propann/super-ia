12fd2396d876a246c8ba08211ceb8c5c86f5e79b
Enzo <54742679+propann@users.noreply.github.com>
2026-08-14 22:35:02 +0200

    feat: export execution leases

--- src/control/index.ts
+++ src/control/index.ts
@@ -1,6 +1,7 @@
 export * from "./cli.js";
 export * from "./control-plane.js";
 export * from "./home.js";
+export * from "./lease-manager.js";
 export * from "./repository-registry.js";
 export * from "./repository-sync.js";
 export * from "./types.js";
