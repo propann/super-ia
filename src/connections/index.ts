89662dacff4c76c31e783838a369da10e8eca6ba
Enzo <54742679+propann@users.noreply.github.com>
2026-08-15 01:04:13 +0200

    feat: export connection dashboard and secret backends

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,4 +1,6 @@
 export * from "./types.js";
 export * from "./catalog.js";
 export * from "./store.js";
+export * from "./dashboard.js";
+export * from "./secret-backends.js";
 export * from "./cli.js";
