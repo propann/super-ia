b88a8b41e14afc53eff48bff25176c6e3f894217
Enzo <54742679+propann@users.noreply.github.com>
2026-08-14 22:24:06 +0200

    feat: export control-plane modules

---
+++ src/control/index.ts
@@ -0,0 +1,6 @@
+export * from "./cli.js";
+export * from "./control-plane.js";
+export * from "./home.js";
+export * from "./repository-registry.js";
+export * from "./repository-sync.js";
+export * from "./types.js";
